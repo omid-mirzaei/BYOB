2161a291c15a4194572272d0ec4596a121865c57
omid mirzaei <omidsp233@gmail.com>
2018-06-11 04:45:44 -0700

    omidamdg

--- App.js
+++ App.js
@@ -1,58 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+import { Container, Header, Content, List, ListItem, Left, Body, Right, Thumbnail, Text , Button , Icon , Title} from 'native-base';
+export default class ListAvatarExample extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='arrow-back' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>مدیریت</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Right>
+        </Header>
+        <Content>
+          <List>
+            <ListItem>
+              
+              <Body>
+                <Text>امید</Text>
+                <Text note>امروز روز جهانی فرار زندانیان ...</Text>
+              </Body>
+              <Thumbnail square size={80} source={require('./assets/img/sqsi.jpg')} />
+            </ListItem>
+          </List>
+          <List>
+            <ListItem>
+              
+              <Body>
+                <Text>امید</Text>
+                <Text note>امروز روز جهانی فرار زندانیان ...</Text>
+              </Body>
+              <Thumbnail square size={80} source={require('./assets/img/sqsi.jpg')} />
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
