00937ebffa4fe124454c076690e9d925b86fea54
omid mirzaei <omidsp233@gmail.com>
2018-06-12 02:19:39 -0700

    بثفزا

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Container, Header, Content, Button, Text } from 'native-base';
 import api from './untilities/api';
 
 export default class HelloWorldApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rovers:[]
+      rovers:[],
+      khales:[],
+      sisi :'string'
     };
+  }
+  
+  sqsq(){
+    alert({khales});  
   }
   componentWillMount(){
     api.getRovers().then((res) => {
@@ -17,18 +23,26 @@ export default class HelloWorldApp extends Component {
       })
     })
   }
+  
   render() {
-    let pushak=[];
+    let names=[];
     console.log("Rovers: ",this.state.rovers);
-    for(let omid =0 ;omid < this.state.rovers.length ; omid++ ){
-      pushak.push(this.state.rovers[omid].name);
+    for(let counter =0 ; counter < this.state.rovers.length ; counter++ ){
+      khales = this.state.rovers[1].name;
+      
+      names.push(
+        <Button rounded light title={this.state.rovers[counter].name} style={{
+          justifyContent:'space-around',
+        }} onPress={ this.sqsq} ><Text>{this.state.rovers[counter].name}</Text></Button>
+      );
     }
     return (
-      
-      <Text>{pushak}</Text>
+      <Container>
+      <Header />
+      <Content>
+          {names}
+      </Content>
+    </Container>
     );
   }
 }
-
-
-  
