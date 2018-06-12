32895be4bc893842f01416bdb1bc1a27b90b9e81
omid mirzaei <omidsp233@gmail.com>
2018-06-12 04:42:04 -0700

    full_name fetch

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Button, Text } from 'native-base';
+import Alert from 'react-native';
+import { Container, Header, Content, List, ListItem, Text , Left, Body, Right, Button, Icon, Title} from 'native-base';
 import api from './untilities/api';
 
 export default class HelloWorldApp extends Component {
@@ -26,22 +27,47 @@ export default class HelloWorldApp extends Component {
   
   render() {
     let names=[];
-    console.log("Rovers: ",this.state.rovers);
     for(let counter =0 ; counter < this.state.rovers.length ; counter++ ){
       khales = this.state.rovers[1].name;
-      
-      names.push(
-        <Button rounded light title={this.state.rovers[counter].name} style={{
-          justifyContent:'space-around',
-        }} onPress={ this.sqsq} ><Text>{this.state.rovers[counter].name}</Text></Button>
-      );
+        for(let counterles =0 ; counterles < this.state.rovers[counter].cameras.length ; counterles++){
+      names.push(<Text style={{color:'#34495e'}}>{this.state.rovers[counter].cameras[counterles].full_name}</Text>);
     }
+    }
+    console.log("Rovers: ",this.state.rovers);
+
     return (
       <Container>
-      <Header />
-      <Content>
-          {names}
-      </Content>
+      <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='arrow-back' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>OMID MIRZAEI</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon name='search' />
+            </Button>
+            <Button transparent>
+              <Icon name='heart' />
+            </Button>
+            <Button transparent>
+              <Icon name='more' />
+            </Button>
+          </Right>
+        </Header>    
+          <Text style={{textAlign:'center',fontSize:22,paddingBottom:10,color:'#e74c3c',paddingTop:5}}>NASA API</Text>
+          <Content>
+          <List dataArray={names}
+            renderRow={(names) =>
+              <ListItem>
+                {names}
+              </ListItem>
+            }>
+          </List>
+        </Content>
     </Container>
     );
   }
